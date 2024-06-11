b42fe2a996802f387aac51c6f1b4a8479b82a992
Thomas Hanke <38633260+ThHanke@users.noreply.github.com>
2024-06-11 20:12:33 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Setting(BaseSettings):
     app_name: str = "CSVtoCSVW"
     admin_email: str = os.environ.get("ADMIN_MAIL") or "csvtocsvw@matolab.org"
     items_per_user: int = 50
-    version: str = "v1.3.0"
+    version: str = "v1.3.2"
     config_name: str = os.environ.get("APP_MODE") or "development"
     openapi_url: str ="/api/openapi.json"
     docs_url: str = "/api/docs"
